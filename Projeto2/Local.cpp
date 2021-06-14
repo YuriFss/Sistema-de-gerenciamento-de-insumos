fa4eb779ca1d15376551109e61ce3d4748819a9b
Yuri Fernandes <43156489+YuriFss@users.noreply.github.com>
2021-06-14 17:42:45 -0300

    Add files via upload

---
+++ Projeto2/Local.cpp
@@ -0,0 +1,40 @@
+#include "Local.h"
+
+Local::Local()
+{
+    nomeLocal = "";
+}
+
+//SETS
+void Local::setInsumo(Insumo *insu)
+{
+    insumos.push_back(insu);
+}
+
+void Local::setNome(string nome)
+{
+    nomeLocal = nome;
+}
+
+//GETS
+vector<Insumo *> Local::getInsumo()
+{
+    return insumos;
+}
+
+string Local::getNome()
+{
+    return nomeLocal;
+}
+
+void Local::excluirInsumo(Insumo *insumo)
+{
+    for (int i = 0; i < insumos.size(); i++)
+    {
+        if (insumos[i]->getNomeProduto() == insumo->getNomeProduto())
+
+            delete insumos[i];
+        insumos.erase(insumos.begin() + i);
+        return;
+    }
+}
